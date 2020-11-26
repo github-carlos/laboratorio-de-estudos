70fe0ec1a2208ddc0348d45cbe1428aef4651dba
Carlos Eduardo Oliveira Bueno <carloseduardo057@gmail.com>
2020-11-26 17:47:28 -0300

    Added verification to see if it its admin

--- flutter/loja_virtual/lib/screens/base/base_screen.dart
+++ flutter/loja_virtual/lib/screens/base/base_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
 import 'package:loja_virtual/models/page_manager.dart';
+import 'package:loja_virtual/models/user_manager.dart';
 import 'package:loja_virtual/screens/home/home_screen.dart';
 import 'package:loja_virtual/screens/products/products_screen.dart';
 import 'package:provider/provider.dart';
@@ -11,19 +12,43 @@ class BaseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Provider(
       create: (_) => PageManager(pageController: _pageController),
-      child: PageView(
-        controller: _pageController,
-        physics: NeverScrollableScrollPhysics(),
-        children: [
-          HomeScreen(),
-          ProductsScreen(),
-          Scaffold(
-            appBar: AppBar(
-              title: Text('Home'),
-            ),
-            drawer: CustomDrawer(),
-          ),
-        ],
+      child: Consumer<UserManager>(
+        builder: (_, userManager, __) {
+          return PageView(
+            controller: _pageController,
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              HomeScreen(),
+              ProductsScreen(),
+              Scaffold(
+                appBar: AppBar(
+                  title: Text('Home3'),
+                ),
+                drawer: CustomDrawer(),
+              ),
+              Scaffold(
+                appBar: AppBar(
+                  title: Text('Home4'),
+                ),
+                drawer: CustomDrawer(),
+              ),
+              if (userManager.adminEnabled) ...[
+                Scaffold(
+                  drawer: CustomDrawer(),
+                  appBar: AppBar(
+                    title: Text('Usuários'),
+                  ),
+                ),
+                Scaffold(
+                  drawer: CustomDrawer(),
+                  appBar: AppBar(
+                    title: Text('Pedidos'),
+                  ),
+                ),
+              ]
+            ],
+          );
+        },
       ),
     );
   }
