815fa94f9ff17a0ad5c32ab306fb2a377de84fd5
github-carlos <carlos@MacBook-Air-de-Carlos.local>
2022-06-29 12:38:08 -0300

    saving changes

--- flutter/reso_coder_clean_arch/lib/main.dart
+++ flutter/reso_coder_clean_arch/lib/main.dart
@@ -6,110 +6,129 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'KbdListener with TextField'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<FocusNode> ListFocusNode = [FocusNode(), FocusNode()];
+  DateTime whenlastchar = DateTime.now();
+  List<String> scanned4 = [];
+  String _receivedtext = "Scanned text here..";
+  final TextEditingController _myTextControler =
+      TextEditingController(text: "");
+  @override
+  void initState() {
+    ListFocusNode.first.requestFocus();
+    super.initState();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            KeyboardListener(
+              key: const Key('KeyboardListener-files'),
+              focusNode: ListFocusNode.first,
+              autofocus: true,
+              onKeyEvent: (event) async {
+                var difference = DateTime.now().difference(whenlastchar);
+                whenlastchar = DateTime.now();
+                if (event.character != null) {
+                  if (difference.inMilliseconds > 1000) {
+                    scanned4.clear();
+                  }
+                  scanned4.add(event.character.toString());
+                  if ((event.character == "\n") ||
+                      (event.character == " ") ||
+                      (event.character == 0x09)) {
+                    String tempo =
+                        scanned4.reduce((first, second) => first + second);
+                    scanned4.clear();
+                    tempo = tempo.trim();
+                    // update
+                    setState(() {
+                      _receivedtext = tempo;
+                    });
+                  }
+                }
+              },
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    _receivedtext,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: _myTextControler,
+                    autofocus: false,
+                    focusNode: ListFocusNode.last,
+                    keyboardType: TextInputType.text,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textInputAction: TextInputAction.done,
+                    onSubmitted: (value) {
+                      print("textfield value: '$value'");
+                      setState(() {
+                        _receivedtext = value;
+                      });
+                      _myTextControler.clear();
+                      FocusScope.of(context).requestFocus(ListFocusNode.first);
+                    },
+                  ),
+                  Row(children: [
+                    TextButton(
+                      child: const Text("KeyboardListener Focus"),
+                      onPressed: () {
+                        setState(() {
+                          FocusScope.of(context)
+                              .requestFocus(ListFocusNode.first);
+                        });
+                      },
+                    ),
+                  ]),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
