e961e45b16ba6576f9eda7bc6a2e22e7015dd0db
github-carlos <carloseduardo057@gmail.com>
2023-07-02 10:25:15 -0300

    creates create account usecase

---
+++ go/microservices_wallet_core/internal/gateway/account.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "microserviceswalletcore/internal/entity"
+
+type AccountGateway interface {
+	FindById(ID string) (*entity.Account, error)
+	Save(a *entity.Account) error
+}
