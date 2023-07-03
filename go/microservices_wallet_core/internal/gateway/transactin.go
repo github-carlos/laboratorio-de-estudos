dcb1c0dd93b6defff02bf20caac07f9f34b166b8
github-carlos <carloseduardo057@gmail.com>
2023-07-03 06:34:58 -0300

    adds createTransactionUseCase

---
+++ go/microservices_wallet_core/internal/gateway/transactin.go
@@ -0,0 +1,7 @@
+package gateway
+
+import "microserviceswalletcore/internal/entity"
+
+type TransactionGateway interface {
+	Create(t entity.Transaction) error
+}
---
+++ go/microservices_wallet_core/internal/usecase/create_transaction/create_transaction.go
@@ -0,0 +1,47 @@
+package createtransaction
+
+import (
+	"microserviceswalletcore/internal/entity"
+	"microserviceswalletcore/internal/gateway"
+)
+
+type CreateTransactionInputDto struct {
+	AccountIDFrom string
+	AccountIDTo   string
+	amount        float64
+}
+type CreateTransactionOutputDto struct {
+	ID string
+}
+
+type CreateTransactionUseCase struct {
+	TransactionGateway gateway.TransactionGateway
+	AccountGateway     gateway.AccountGateway
+}
+
+func NewCreateTransactionUseCase(tg gateway.TransactionGateway, ag gateway.AccountGateway) CreateTransactionUseCase {
+	return CreateTransactionUseCase{}
+}
+
+func (uc CreateTransactionUseCase) Execute(input CreateTransactionInputDto) (*CreateTransactionOutputDto, error) {
+	accountFrom, err := uc.AccountGateway.FindById(input.AccountIDFrom)
+	if err != nil {
+		return nil, err
+	}
+	accountTo, err := uc.AccountGateway.FindById(input.AccountIDTo)
+	if err != nil {
+		return nil, err
+	}
+	transaction, err := entity.NewTransaction(accountFrom, accountTo, input.amount)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = uc.TransactionGateway.Create(*transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateTransactionOutputDto{ID: transaction.ID}, nil
+}
